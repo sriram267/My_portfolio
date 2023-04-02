623b67d94161a9a6a70a9e506cccf859a9cd535b
sriram <vssriram7@gmail.com>
2023-04-03 02:39:03 +0530

    .

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -23,6 +23,13 @@ import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
+import work1 from '../assets/work1.png';
+import work2 from '../assets/work2.png';
+import work3 from '../assets/work3.png';
+import work4 from '../assets/work4.png';
+import work5 from '../assets/work5.png';
+import work6 from '../assets/work6.png';
+
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
@@ -59,6 +66,12 @@ export default {
   about02,
   about03,
   about04,
+  work1,
+  work2,
+  work3,
+  work4,
+  work5,
+  work6,
   profile,
   circle,
   logo,
--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,11 +1,151 @@
-import React from 'react'
-import "./Work.scss"
+import React, { useState } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+import { images } from "../../constants"
+import './Work.scss';
+
 const Work = () => {
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  const works = [
+    { 
+      title: 'Restaurant Website',
+      description: 'This is a sample project for testing purposes.',      
+      imgUrl: images.work1,      
+      tags: ['React JS'],
+      projectLink: 'https://sriz-kitchen.netlify.app',
+      codeLink: 'https://github.com/example/sample-project-1',
+    },
+    {
+      title: 'React Admin Dashboard',
+      description: 'This is another sample project for testing purposes.',
+      imgUrl: images.work3,
+      tags: ['React JS'],
+      projectLink: 'https://react-admin-dashboard-2023.netlify.app',
+      codeLink: 'https://github.com/example/sample-project-2',
+    },
+    {
+      title: 'Portfolio Website',
+      description: 'This is another sample project for testing purposes.',
+      imgUrl: images.work2,
+      tags: ['React JS'],
+      projectLink: 'https://My-Portfolio-Sriram-VS.netlify.app',
+      codeLink: 'https://github.com/example/sample-project-2',
+    },
+    {
+      title: 'Passenger Counter App',
+      description: 'This is another sample project for testing purposes.',
+      imgUrl: images.work4,
+      tags: ['JavaScript'],
+      projectLink: undefined,
+      codeLink: 'https://github.com/sriram267/counter-app',
+    },
+    {
+      title: 'BlackJack Game',
+      description: 'This is another sample project for testing purposes.',
+      imgUrl: images.work5,
+      tags: ['JavaScript'],
+      projectLink: undefined,
+      codeLink: 'https://github.com/sriram267/black-jack-game',
+    },
+    {
+      title: 'Lead Tracker',
+      description: 'This is another sample project for testing purposes.',
+      imgUrl: images.work6,
+      tags: ['JavaScript'],
+      projectLink: undefined,
+      codeLink: 'https://github.com/sriram267/lead-tracker',
+    },
+  ];
+
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
+
+  useState(() => setFilterWork(works));
   return (
-    <div>
-      
+    <div className="app__works">
+      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+
+      <div className="app__work-filter">
+        {['React JS', 'JavaScript', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div
+              className="app__work-img app__flex"
+            >
+              <img src={(work.imgUrl)} alt={work.name} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
 export default Work
